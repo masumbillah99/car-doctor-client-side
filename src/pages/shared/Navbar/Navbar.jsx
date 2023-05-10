dd6028ea68908b96634948a5dfa71d81a5f4a963
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-05-10 09:40:52 +0600

    project setup, add navbar and carousel

---
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.svg";
+
+const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/services">Services</Link>
+      </li>
+      <li>
+        <Link to="/blog">Blog</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-start h-28">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navItems}
+            </ul>
+          </div>
+          <Link className="btn btn-ghost normal-case text-xl">
+            <img src={logo} className="-mt-5" alt="logo" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-bold">{navItems}</ul>
+        </div>
+        <div className="navbar-end">
+          <Link className="btn btn-outline btn-error">Appointment</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }, 
+            {
+                path: '',
+                
+            }
+        ]
+    }
+])
+
+export default router;
