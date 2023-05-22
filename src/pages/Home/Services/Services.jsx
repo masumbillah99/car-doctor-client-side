7e61d827d98050c348c18f0a89b38339fbbb148d
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-05-22 12:44:38 +0600

    implement sorting and search system

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,15 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [ascending, setAscending] = useState(true);
+  const [search, setSearch] = useState("");
+  const searchRef = useRef(null);
 
   useEffect(() => {
-    fetch("https://car-doctor-server-gilt-rho.vercel.app/services")
+    fetch(
+      `http://localhost:5000/services?search=${search}&sort=${
+        ascending ? "ascending" : "descending"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [ascending, search]);
+
+  const handleSearch = () => {
+    // console.log(searchRef.current.value);
+    setSearch(searchRef.current.value);
+  };
 
   return (
     <div className="mb-20">
@@ -21,6 +33,38 @@ const Services = () => {
           humour, or randomised words which don&lsquo;t look even slightly
           believable.{" "}
         </p>
+        <div className="form-control mt-3">
+          <div className="input-group justify-center">
+            <input
+              type="text"
+              ref={searchRef}
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <button
+          onClick={() => setAscending(!ascending)}
+          className="btn btn-secondary mt-5"
+        >
+          {ascending ? "Price High to Low" : "Price Low to High"}
+        </button>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mx-10 lg:mx-0">
         {services.map((service) => (
